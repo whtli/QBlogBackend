0bca0550ee876fcb7bb7a149f264a2920ea82dd4
whtli <3239349119@qq.com>
2022-11-16 20:58:14 +0800

    p3c

--- src/main/java/cn/li98/blog/service/BlogService.java
+++ src/main/java/cn/li98/blog/service/BlogService.java
@@ -16,6 +16,7 @@ public interface BlogService extends IService<Blog> {
      * 新发布博客
      *
      * @param blog
+     * @return 1：创建成功；0：创建失败
      */
     int createBlog(Blog blog);
 
@@ -23,6 +24,7 @@ public interface BlogService extends IService<Blog> {
      * 更新已有博客
      *
      * @param blog
+     * @return 1：更新成功；0：更新失败
      */
     int updateBlog(Blog blog);
 }
