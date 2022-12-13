ca68fd7690dac662617ed67e384551c538db2407
Qiang Li <3239349119@qq.com>
2022-12-13 22:14:38 +0800

    删除无效的配置，整合冗余的代码文件

--- src/main/java/cn/li98/blog/controller/admin/UserController.java
+++ src/main/java/cn/li98/blog/controller/admin/UserController.java
@@ -1,8 +1,9 @@
-package cn.li98.blog.controller;
+package cn.li98.blog.controller.admin;
 
-import cn.li98.blog.model.dto.LoginDTO;
+
 import cn.li98.blog.common.Result;
 import cn.li98.blog.model.User;
+import cn.li98.blog.model.dto.LoginDTO;
 import cn.li98.blog.service.UserService;
 import cn.li98.blog.utils.TokenUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -11,20 +12,30 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: whtli
- * @date: 2022/11/10
- * @description:
+ * @date: 2022/11/09
+ * @description: UserController
  */
 @Slf4j
-@CrossOrigin
 @RestController
 @RequestMapping("/admin")
-public class AccountController {
+public class UserController {
     @Autowired
     UserService userService;
 
+    @GetMapping("/test")
+    public Result updateUserName(@RequestParam String name) {
+        User currentUser = TokenUtils.getCurrentUser();
+        System.out.println("获取当前用户信息 ====== " + currentUser.getUsername());
+
+        Map<String, String> data = new HashMap<>(1);
+        data.put("userBar", "Hello  " + name);
+        return Result.succ("测试连接成功!", data);
+    }
 
     @PostMapping("/login")
     public Result login(@Validated @RequestBody LoginDTO loginDTO, HttpServletResponse response) {
